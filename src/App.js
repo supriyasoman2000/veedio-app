6b21632fcb025f26d2d5ada315e32ca5b621a8fb
supriya <supriyasoman2000@gmail.com>
2023-09-27 15:23:10 +0530

    veedio app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import LandingPage from './Pages/LandingPage';
+import Home from './Pages/Home';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Veedio App</h1>
+      <LandingPage/>
+      <Home/>
+
     </div>
   );
 }
